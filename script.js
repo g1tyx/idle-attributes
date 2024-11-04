2a68ada7ac8ef1098b835c23cb90b683f3122a5f
zhaolinxu <zhaolinxu@sina.cn>
2024-11-04 11:58:37 +0800

    hh

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let saveInterval;
 function saveLoop() {
   saveInterval = setInterval(()=> {
     saveGameState();
-    console.log(`${Date.now()}: Game Saved.`);
+    // console.log(`${Date.now()}: Game Saved.`);
   }, saveFrequency);
 }
 function saveGameState() {
